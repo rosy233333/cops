4142033771cf14c4e73116723fd18061c8175f7b
zflcs <1491657576@qq.com>
2025-04-03 16:16:57 +0800

    Change link script position.

--- build.rs
+++ build.rs
@@ -1,10 +1,10 @@
 fn main() {
     let pwd = std::env::current_dir().unwrap();
     println!("cargo:rerun-if-changed=src/*");
-    println!("cargo:rerun-if-changed=./cops.lds");
+    println!("cargo:rerun-if-changed=./link.lds");
     println!("cargo:rerun-if-changed=./build.rs");
     println!("cargo:rustc-cdylib-link-arg=-fuse-ld=lld");
-    println!("cargo:rustc-link-arg=-T{}/cops.lds", pwd.display());
+    println!("cargo:rustc-link-arg=-T{}/link.lds", pwd.display());
     println!("cargo:rustc-link-arg=-soname=libcops.so");
     println!("cargo:rustc-link-arg=-fPIC");
 }
